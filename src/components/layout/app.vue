68a66e133fe9840243f252ef04e073cb13e1e31e
Leandro Henrique Reis <emtudo@gmail.com>
2017-12-22 18:51:26 -0200

    first commit

---
+++ src/components/layout/app.vue
@@ -0,0 +1,28 @@
+<script>
+import RootHeader from './header/header'
+import RootFooter from './footer/footer'
+import SibebarLeft from './sidebar/left'
+import SibebarRight from './sidebar/right'
+
+export default {
+  name: 'app',
+  components: {
+    RootHeader,
+    RootFooter,
+    SibebarLeft,
+    SibebarRight
+  }
+}
+</script>
+
+
+<template>
+  <div id="app" class="wrapper">
+    <root-header />
+    <sibebar-left />
+    <router-view/>
+    <root-footer></root-footer>
+    <sibebar-right />
+    <div class="control-sidebar-bg"></div>
+  </div>
+</template>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import App from './components/layout/app'
+import router from './router'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
